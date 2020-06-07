fe0ab616f069c59b2cd8c157a2790abb6f8d590e
Mikiyas Belhu <mikiyasbelhu@gmail.com>
2020-06-07 16:29:17 -0500

    add shared components

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import FormErrorMessages from "./FormErrorMessages.vue";
+import InternalErrorAlert from "./InternalErrorAlert.vue";
+import NetworkErrorAlert from "./NetworkErrorAlert.vue";
+
+export {
+  NetworkErrorAlert,
+  InternalErrorAlert,
+  FormErrorMessages,
+};
---
+++ src/shared/mixins/table.mixin.js
@@ -0,0 +1,56 @@
+const debounce = require("lodash.debounce");
+
+const tableMixin = {
+  watch: {
+    options: {
+      handler() {
+        this.loadData();
+      },
+      deep: true
+    },
+    search() {
+      this.doSearch();
+    }
+  },
+  data() {
+    return {
+      totalItems: 0,
+      items: [],
+      loading: false,
+      options: {},
+      search: ""
+    };
+  },
+  methods: {
+    doSearch: debounce(async function s() {
+      if (this.search) {
+        this.loading = true;
+
+        const { page, itemsPerPage, sortBy, sortDesc } = this.options
+        const sortColumn = sortBy.length != 0 ? sortBy[0]: this.headers[0].value;
+        const sortDirection = sortDesc.length != 0 ? sortDesc[0]: false;
+        
+        const res = await this.resource.search(this.search, page-1, itemsPerPage, sortColumn, sortDirection);
+        this.items = res.content;
+        this.totalItems = res.totalElements;
+        this.loading = false;
+
+      } else {
+        this.loadData();
+      }
+    }, 500),
+    async loadData() {
+      this.loading = true;
+
+      const { page, itemsPerPage, sortBy, sortDesc } = this.options
+      const sortColumn = sortBy.length != 0 ? sortBy[0]: this.headers[0].value;
+      const sortDirection = sortDesc.length != 0 ? sortDesc[0]: false;
+      
+      const res = await this.resource.all(page-1, itemsPerPage, sortColumn, sortDirection);
+      this.items = res.content;
+      this.totalItems = res.totalElements;
+      this.loading = false;
+    }
+  }
+};
+export default tableMixin;
