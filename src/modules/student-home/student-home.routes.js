d7569a12e4f2410ac08b7e51707529e937e866f7
Mikiyas Belhu <mikiyasbelhu@gmail.com>
2020-06-14 13:46:06 -0500

    add course list

--- src/modules/student-home/student-home.routes.js
+++ src/modules/student-home/student-home.routes.js
@@ -1,19 +1,38 @@
 import GroupChat from "./GroupChat.vue";
 import GroupList from "./GroupList.vue";
+import CourseList from "./CourseList.vue";
+import RequestList from "./RequestList.vue";
 
 export default [
   {
-    path: "group/:tutorialGroupId",
+    path: "group-chat",
     name: "group-chat",
     component: GroupChat,
     meta: {
       allowedRoles: ["Administrator"]
-    }
+    },
+    props: true
   },
   {
     path: "group-list",
     name: "group-list",
     component: GroupList,
+    meta: {
+      allowedRoles: ["Administrator"]
+    }
+  },
+  {
+    path: "student-course-list",
+    name: "student-course-list",
+    component: CourseList,
+    meta: {
+      allowedRoles: ["Administrator"]
+    }
+  },
+  {
+    path: "student-request-list",
+    name: "student-request-list",
+    component: RequestList,
     meta: {
       allowedRoles: ["Administrator"]
     }
